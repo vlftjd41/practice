437d89bf016636a57a63d0f308ea72490774e111
java <java@localhost>
2015-02-01 03:37:07 +0900

    new!2

--- JavaJungsuk2/src/chapter6/CarTest3.java
+++ JavaJungsuk2/src/chapter6/CarTest3.java
@@ -4,5 +4,7 @@ public class CarTest3 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, this is my first git-project!");
+		System.out.println("Hello, this is my second git-project!");
+		
 	}
 }
